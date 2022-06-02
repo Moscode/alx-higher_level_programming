5a21d7a020668dd38a02e4c09b5f780abafab3c3
Moscode <odunoshomoses@gmail.com>
2022-06-02 09:51:02 -0700

    [fix]: make sure it is the program running directly

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-result = add(a, b)
-print('{:d} + {:d} = {:d}'.format(a, b, result))
+if __name == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a, b)
+    print('{:d} + {:d} = {:d}'.format(a, b, result))
