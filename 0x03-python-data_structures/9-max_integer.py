42e99e03dba0994722683f4b5bae688fa21c7b3f
Moscode <odunoshomoses@gmail.com>
2022-06-07 06:31:28 -0700

    [fix]: handling empty string

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 def max_integer(my_list=[]):
-    if(len(my_list) == 0):
+    if(!my_list):
         return None
     max_num = my_list[0]
     for i in range(len(my_list) - 1):
