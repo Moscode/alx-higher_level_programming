1c9b1cc6158e387b2d760dd643bdad4029c6b534
Moscode <odunoshomoses@gmail.com>
2022-06-07 06:41:44 -0700

    [fix]: cover all elements of the list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if(my_list):
+    if my_list:
         max_num = my_list[0]
-        for i in range(len(my_list) - 1):
-            if(max_num < my_list[i]):
+        for i in range(len(my_list)):
+            if max_num < my_list[i]:
                 max_num = my_list[i]
         return max_num
     return None
