108fb55eae5c4a3e37545371f4178c4ffb8f5da4
Moscode <odunoshomoses@gmail.com>
2022-06-20 11:22:19 -0700

    [fix]: handling IndexError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,6 +5,8 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[index]), end="")
             i += 1
+        except IndexError:
+            break
         except Exception:
             continue
     print("")
