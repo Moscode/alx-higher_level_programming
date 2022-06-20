6470efd9132393f8cb0bb2408ab205583120b2cc
Moscode <odunoshomoses@gmail.com>
2022-06-20 14:47:09 -0700

    [fix]: Left IndexError unhandled

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,9 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[index]), end="")
             i += 1
-        except IndexError:
-            break
-        except Exception:
+        except (ValueError, TypeError):
             pass
     print("")
     return i
