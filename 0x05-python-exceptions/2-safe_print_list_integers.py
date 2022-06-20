720d4697c7f3bd225438af4486e9e98cdb7e3c89
Moscode <odunoshomoses@gmail.com>
2022-06-20 11:24:35 -0700

    [fix]: use pass instead of continue

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,6 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
         except IndexError:
             break
         except Exception:
-            continue
+            pass
     print("")
     return i
