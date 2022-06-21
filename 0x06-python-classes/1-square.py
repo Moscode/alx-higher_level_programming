91a596405adb8c96b72a2a318424994f07577c04
Moscode <odunoshomoses@gmail.com>
2022-06-21 10:14:15 -0700

    [fix]: added comment for description

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+Square class module with private instance attribute - size
+"""
+
+
 class Square:
     def __init__(self, size=0):
+        """
+        private instance
+        """
         self.__size = size
