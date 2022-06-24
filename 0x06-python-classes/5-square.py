003f42e3a411d192e84d6410931e878d7c34a1e3
Moscode <odunoshomoses@gmail.com>
2022-06-24 04:45:09 -0700

    [feat]: printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This is Square module with new feature
+"""
+
+
+class Square:
+    """
+    private instance attribute & error handling
+    """
+    def __init__(self, size=0):
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    """
+    getter & setter
+    """
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if (type(value) != int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    """
+    public instance method
+    """
+    def area(self):
+        return self.__size ** 2
+    def my_print(self):
+        if (self.__size):
+            for i in range(self.__size):
+                print("#" * self.__size)
+        else:
+            print(" ")
