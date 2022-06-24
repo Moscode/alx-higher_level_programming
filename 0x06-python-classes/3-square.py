6f28784e75defc55abb4d31db52b800c7b51a228
Moscode <odunoshomoses@gmail.com>
2022-06-24 05:47:30 -0700

    [fix]: recovering coddes for file

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,3 +2,21 @@
 """
 S
 """
+
+
+class Square:
+    """
+    private instance attribute & error handling
+    """
+    def __init__(self, size=0):
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    """
+    new feature: public instance method
+    """
+    def area(self):
+        return self.__size ** 2
