d21e2908590bae51f827b01425866a9a35107d46
Moscode <odunoshomoses@gmail.com>
2022-06-29 16:12:52 -0700

    [fix]: use slot instead of the initial setattr

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-class LockedClass():
-    """Class and instances cannot dynamically create attribute
-        except first_name
-    """
-    def __init__(self):
-        """
-        set inial attribute
-        """
-        first_name = "Moses"
-    def __setattr__(self, first_name, value):
-        """if attribute is not know don't use it"""
+"""
+Defines class with no class or object attribute
+"""
 
-        if not first_name in self.__dict__:
-            return
+
+class LockedClass():
+    """
+    prevent user from creating new instance attribute dynamically
+    unless attribute is "first_name
+    """
+
+    __slots__ = "first_name"
