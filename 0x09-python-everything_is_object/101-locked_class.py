7d27f56de7e74e16a18b3d712877fbc07a9ace95
Moscode <odunoshomoses@gmail.com>
2022-06-29 16:01:49 -0700

    [feat]:added comment

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,8 +3,12 @@ class LockedClass():
         except first_name
     """
     def __init__(self):
+        """
+        set inial attribute
+        """
         first_name = "Moses"
-
     def __setattr__(self, first_name, value):
+        """if attribute is not know don't use it"""
+
         if not first_name in self.__dict__:
             return
