1cc8a4797918523a8c2808bc6e5063e21f687400
Moscode <odunoshomoses@gmail.com>
2022-07-04 03:22:31 -0700

    Adjusted the docstring

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""
-    Add two integers and returns its sum
-"""
+""" Add two integers
+    Args:
+        a (int): first  integer, b (int): second integer
+    Return:
+        Sum of the two integers"""
 
 
 def add_integer(a, b=98):
-    """ Add two integers
-        Args:
-            a (int): first  integer
-            b (int): second integer
-        Return:
-            Sum of the two integers
+    """ 
+    Takes two values and return their sum 
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
