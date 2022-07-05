51c349da13f2ed743e7263419c01077c161f2c70
Moscode <odunoshomoses@gmail.com>
2022-07-05 07:13:32 -0700

    [fix]: type error

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 """
-Function to check if obj is an instance of, or instance of a class inherited from
+Function to check if obj is an instance of, or instance of a class inherited
 """
 
 
 def is_kind_of_class(obj, a_class):
-    """ Returns True if obj is from a_class or is inheritance class
+    """ Returns True if obj is from a_cilass or is inheritance class
         Args:
             obj: value to validate
             a_class: value to compare with
