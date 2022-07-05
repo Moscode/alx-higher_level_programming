d0ccb23390e05a25d2e2070ded4f66ebefc00cbd
Moscode <odunoshomoses@gmail.com>
2022-07-05 03:58:31 -0700

    [feat]: To check for exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Method to determine if it's an instance
+"""
+def is_same_class(obj, a_class):
+    """
+        Args:
+            obj (object): to test
+            a_class (class): to compare to
+        Returns (Boolean): True or False
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
