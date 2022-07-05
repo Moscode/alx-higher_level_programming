5e9eeb209caf83a1bea029d90aac5e66c0a5e3c2
Moscode <odunoshomoses@gmail.com>
2022-07-05 07:47:27 -0700

    [feat]: only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Function that returns True if obj is an instance else return False
+"""
+
+
+def inherits_from(obj, a_class):
+    """Check if obj is inherited from a_class directly or indirectly
+        Args:
+            obj: object to check
+            a_class: class to compare to
+        Returns: True if same other False
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
