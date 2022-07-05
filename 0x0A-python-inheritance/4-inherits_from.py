8cafadd7797c1e8a3c4f8db6d572132d09865bfe
Moscode <odunoshomoses@gmail.com>
2022-07-05 07:52:01 -0700

    [fix]: logic error

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -12,6 +12,6 @@ def inherits_from(obj, a_class):
             a_class: class to compare to
         Returns: True if same other False
     """
-    if isinstance(obj, a_class):
+    if type(obj) == a_class:
         return True
     return False
