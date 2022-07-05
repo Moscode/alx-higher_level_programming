21b242eca77199d350788599235aa1c9a816f4bd
Moscode <odunoshomoses@gmail.com>
2022-07-05 07:06:44 -0700

    [feat]: same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/pytho""""
+"""
+Function to check if obj is an instance of, or instance of a class inherited from
+"""
+def is_kind_of_class(obj, a_class):
+    """ Returns True if obj is from a_class or is inheritance class
+        Args:
+            obj: value to validate
+            a_class: value to compare with
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
