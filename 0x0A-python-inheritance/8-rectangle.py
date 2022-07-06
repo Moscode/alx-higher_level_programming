8ff6c7668409085b7f21ef46aafdcaa12cbe2fb7
Moscode <odunoshomoses@gmail.com>
2022-07-05 17:22:44 -0700

    [fix]: importing the superclass from it file

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,26 +1,6 @@
 #!/usr/bin/python3
-
-"""
-class with public instance method
-"""
-
-
-class BaseGeometry:
-    """
-    BaseGeometry with a method
-    """
-
-    def area(self):
-        """
-        Area method doing nothing yet
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+"""Import class from another class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 """
