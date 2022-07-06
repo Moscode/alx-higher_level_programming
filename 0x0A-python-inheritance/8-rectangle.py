964da07521836fc2ff86ca1c7e9b0043379dcca6
Moscode <odunoshomoses@gmail.com>
2022-07-05 17:15:11 -0700

    [fix]: made pycodestyle compliant

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -9,20 +9,24 @@ class BaseGeometry:
     """
     BaseGeometry with a method
     """
+
     def area(self):
         """
         Area method doing nothing yet
         """
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
+
 """
 class Rectangle that inherited from BaseGeometry
 """
+
 
 class Rectangle(BaseGeometry):
     """
