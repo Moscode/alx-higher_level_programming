027a265a6b0104f00b9623aed24858c5ba81241e
Moscode <odunoshomoses@gmail.com>
2022-07-08 02:53:23 -0700

    appending contents to json file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -14,7 +14,9 @@ Saving command line argument into a json file without the element at index 0
 """
 
 
-save_to_json_file(sys.argv, "add_item.json")
-oriList = load_from_json_file("add_item.json")
-del oriList[0]
-save_to_json_file(oriList, "add_item.json")
+try:
+    contents = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    contents = []
+contents.extend(sys.argv[1:])
+save_to_json_file(contents, "add_item.json")
