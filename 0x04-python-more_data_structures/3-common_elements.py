b44cf38ac13d94886253f55e786fc9b836089350
Moscode <odunoshomoses@gmail.com>
2022-07-09 16:36:25 -0700

    [feat]: present in both sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Element common to both set_1 and set_2
+"""
+
+
+def common_elements(set_1, set_2):
+    """ common in both set_1 and set_2
+        Args:
+            set_1 (set): first set
+            set_2 (Set): second set
+        Returns: common element(s)
+    """
+    return set_1 & set_2
