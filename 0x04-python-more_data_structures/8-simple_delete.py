645fcbad176435254ffa2f6b11383f8f08afb1df
Moscode <odunoshomoses@gmail.com>
2022-07-10 04:59:55 -0700

    [feat]: simple delete by key

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Function to delete key from dictionary
+"""
+
+
+def simple_delete(a_dictionary, key=""):
+    """ Delete key from a_dictionary
+        Args:
+            a_dictionary (dict): where to delete key from
+            key (string): what should be deleted
+    """
+    try:
+        del a_dictionary[key]
+        return a_dictionary
+    except:
+        return a_dictionary
