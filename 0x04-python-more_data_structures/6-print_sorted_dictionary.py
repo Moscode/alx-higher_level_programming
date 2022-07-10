3a3e572a221927b36c924a7186adacc94b290d60
Moscode <odunoshomoses@gmail.com>
2022-07-09 18:58:48 -0700

    [fix]: print the sorted dictionary line by line not as dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -15,7 +15,5 @@ def print_sorted_dictionary(a_dictionary):
     sorted_key = sorted(key_list)
     for key in sorted_key:
         value_list.append(a_dictionary[key])
-    new_dict = {}
     for i in range(len(sorted_key)):
-        new_dict[sorted_key[i]] = value_list[i]
-    print(new_dict)
+        print("{}:{}".format(sorted_key[i], value_list[i]))
