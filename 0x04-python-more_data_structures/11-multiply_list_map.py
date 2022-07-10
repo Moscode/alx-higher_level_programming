a0c7299e6101702694816bfbe17bcca3260cf744
Moscode <odunoshomoses@gmail.com>
2022-07-10 06:25:29 -0700

    [fix]: max line - 3 so no comment

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
-"""
-Multiply with each element of list with using for loops
-"""
-
-
 def multiply_list_map(my_list=[], number=0):
-    """ Multiply each element of list with number
-        Args:
-            my_list (list): list to compute on
-            number (int): number to multiply by each element
-        Returns (list): multiplied by number
-    """
     return list(map(lambda x: x * number, my_list))
