e43a2646d435ec05cf1abe3f2436359532acc862
Moscode <odunoshomoses@gmail.com>
2022-08-14 23:06:53 -0700

    [feat]: fixed value insertion

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,3 @@
 --  a script that creates a table second_table in the database
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT) 
-VALUES (1, "John", 10), (2, "ALEX", 3), (3, "Bob", 14), (4, "George", 8)
+VALUES ((1, "John", 10), (2, "ALEX", 3), (3, "Bob", 14), (4, "George", 8))
