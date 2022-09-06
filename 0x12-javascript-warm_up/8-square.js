4ac6111b825ee7a337bf4e9b6fc3a3f82ae03ded
Moscode <odunoshomoses@gmail.com>
2022-09-05 21:00:50 -0700

    [feat]:Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const arg = process.argv[2];
+if (isNaN(arg)) console.log('Missing Size');
+const intVersion = Math.floor(arg);
+for (let i = 0; i < intVersion; i++) {
+  const temp = [];
+  for (let j = 0; j < intVersion; j++) {
+    temp.push('X');
+  }
+  console.log(temp.join(''));
+}
