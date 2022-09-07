d67a736182c2694b89f28cccee23cb5488c39521
Moscode <odunoshomoses@gmail.com>
2022-09-07 14:44:48 -0700

    [feat]:Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (!isNaN(w) && !isNaN(h) && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      const store = [];
+      for (let j = 0; j < this.width; j++) {
+        store.push('X');
+      }
+      console.log(`${store.join('')}`);
+    }
+  }
+
+  rotate () {
+    const temp_w = this.width;
+    this.width = this.height;
+    this.height = temp_w;
+  }
+  
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
+}
+module.exports = Rectangle;
