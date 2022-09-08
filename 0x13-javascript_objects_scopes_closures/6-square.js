36e062a57614171955ca4a46f3a3a1bf16caac25
Moscode <odunoshomoses@gmail.com>
2022-09-08 00:54:24 -0700

    [fix]: more dynamic

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,12 +2,10 @@
 const Square = require('./5-square.js');
 class Square1 extends Square {
   charPrint (c) {
-    if (c) {
-      for (let i = 0; i < this.width; i++) {
-        console.log('C'.repeat(this.height));
-      }
-    } else {
+    if (!c) {
       this.print();
+    } else {
+        for (let i = 0; i < this.width; i++) console.log(c.repeat(this.height))
     }
   }
 }
