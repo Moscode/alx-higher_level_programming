bf51e057d4dace0445b89a6e50b2402c12bb8ef1
Moscode <odunoshomoses@gmail.com>
2022-09-08 07:24:51 -0700

    [fix]: strictly equal to

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,15 +3,15 @@ const argLength = process.argv.length;
 const arg = process.argv;
 const storeInput = [];
 if (argLength < 4) {
-  console.log(0)
+  console.log(0);
 }
 for (let j = 2; j < argLength; j++) {
   storeInput.push(arg[j]);
 }
 storeInput.sort();
 for (let i = storeInput.length - 2; i > 1; i--) {
-    if (storeInput[i] != storeInput[argLength - 1]) {
-      console.log(storeInput[i]);
-      break;
+  if (storeInput[i] !== storeInput[argLength - 1]) {
+    console.log(storeInput[i]);
+    break;
   }
 }
