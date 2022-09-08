89f3ec7e2e2b9de9623b742674889b8e04935dd1
Moscode <odunoshomoses@gmail.com>
2022-09-08 00:43:26 -0700

    [fix]: used super

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -3,20 +3,7 @@ const Rectangle = require('./4-rectangle.js');
 
 class Square extends Rectangle {
   constructor (size) {
-    super();
-    this.width = size;
-    this.height = size;
-  }
-
-  print () {
-    for (let i = 0; i < this.width; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  double () {
-    this.width = 2 * this.width;
-    this.height = 2 * this.height;
+    super(size, size);
   }
 }
 module.exports = Square;
