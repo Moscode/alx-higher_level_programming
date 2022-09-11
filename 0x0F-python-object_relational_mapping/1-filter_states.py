1a57814791d48c13ae8cfc405d5aabe5811f019f
Moscode <odunoshomoses@gmail.com>
2022-09-11 14:39:24 -0700

    [feat]: Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+lists all states from the database hbtn_0e_0_usa
+"""
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    userInput = sys.argv[1]
+    passwdInput = sys.argv[2]
+    dbInput = sys.argv[3]
+    db = MySQLdb.connect(
+            host='localhost', user=userInput,
+            passwd=passwdInput, db=dbInput,
+            port=3306, charset='utf8')
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
