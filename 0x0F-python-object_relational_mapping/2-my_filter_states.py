d6a73b7270744c35d18de16966410f25d2c26aaa
Moscode <odunoshomoses@gmail.com>
2022-09-12 18:00:22 -0700

    [Feat]:Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Use MySQLdb to query a database for state that is
+passed as argument.
+"""
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    userInput = sys.argv[1]
+    passwdInput = sys.argv[2]
+    dbInput = sys.argv[3]
+    stateInput = sys.argv[4]
+    db = MySQLdb.connect(
+            host='localhost', user=userInput,
+            passwd=passwdInput, db=dbInput,
+            port=3306, charset='utf8')
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states
+            WHERE name=binary '{}' ORDER BY id ASC""".format(stateInput))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
